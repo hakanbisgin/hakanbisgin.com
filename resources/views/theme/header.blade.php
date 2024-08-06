bc221ee2625a56566d86d74259f7a8af9ea82c00
hakanbisgin <hakanbisgin.red@gmail.com>
2024-08-06 23:43:41 +0300

    sitename correction

--- resources/views/theme/header.blade.php
+++ resources/views/theme/header.blade.php
@@ -22,7 +22,7 @@
         class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
         <div class="relative w-full max-w-2xl px-2 lg:max-w-7xl">
             <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                <div class="flex lg:justify-center lg:col-start-2" style="font-size: xxx-large">
+                <div class="flex lg:justify-center lg:col-start-2 text-xl lg:text-7xl" >
                     <a href="/">
                         HAKANBISGIN
                     </a>
