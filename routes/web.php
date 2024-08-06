09334f46437cf8b198f577552d6a19aa7a3c107e
hakanbisgin <hakanbisgin.red@gmail.com>
2024-08-07 00:25:43 +0300

    transparent button fixed. Post model,controller and migration defined

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::prefix('admin')->group(function () {
+       Route::resource('posts',PostController::class);
+    })->name('admin');
 });
 
 require __DIR__ . '/auth.php';
