56243514dab7658cfcfa751927cfca63c477c406
hakanbisgin <hakanbisgin.red@gmail.com>
2024-08-07 04:45:56 +0300

    Footer link updated.

--- resources/views/theme/footer.blade.php
+++ resources/views/theme/footer.blade.php
@@ -2,7 +2,7 @@
 </main>
 
 <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-    <a href="hakanbisgin.com" style="font-size: xx-large"> Hakan Bisgin </a>
+    <a href="https://www.hakanbisgin.com" style="font-size: xx-large"> Hakan Bisgin </a>
     <br>
     Build with Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
 </footer>
